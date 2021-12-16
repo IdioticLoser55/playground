4ada64b54a029370c40d5501c3b63583c7b5a1ca
IdioticLoser55 <idioticloser55@gmail.com>
2021-12-16 10:55:45 +0000

    got it printing hello world.

--- libgdx/helloWorld/core/src/failure/helloworld/HelloWorld.java
+++ libgdx/helloWorld/core/src/failure/helloworld/HelloWorld.java
@@ -2,17 +2,21 @@ package failure.helloworld;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class HelloWorld extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+    BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+        font = new BitmapFont();
 	}
 
 	@Override
@@ -20,6 +24,7 @@ public class HelloWorld extends ApplicationAdapter {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
+        font.draw(batch, "Hello World!", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
 		batch.end();
 	}
 	
@@ -27,5 +32,6 @@ public class HelloWorld extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+        font.dispose();
 	}
 }
