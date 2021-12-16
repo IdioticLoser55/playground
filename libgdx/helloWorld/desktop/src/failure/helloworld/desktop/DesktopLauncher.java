631b54251fd99669cd9b55d9b6d8a47baaad734e
IdioticLoser55 <idioticloser55@gmail.com>
2021-12-16 09:44:39 +0000

    built basic project.

---
+++ libgdx/helloWorld/desktop/src/failure/helloworld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package failure.helloworld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import failure.helloworld.HelloWorld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HelloWorld(), config);
+	}
+}
