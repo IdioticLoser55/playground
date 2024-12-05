796755f27c0704605a582be51183aabd93e4de3f
Idiot_icLoser55 <IdioticLoser55@gmail.com>
2024-12-05 00:24:19 +0000

    made it faster.

--- adventOfCode/2024/day_04/src/main.rs
+++ adventOfCode/2024/day_04/src/main.rs
@@ -30,66 +30,51 @@ fn part1 (input: &str) -> String {
     let rpattern: &[u8] = &pattern.iter().rev().map(|&c| c.clone()).collect::<Vec<u8>>();
     let pattern_size = pattern.len();
 
-    let mut slices_to_search:Vec<Vec<usize>> = Vec::new();
+    let mut slices_to_search:Vec<Box<dyn Iterator<Item = usize>>> = Vec::with_capacity(2 * (width + height));
 
     // horizontal slices.
     for y in (0..height*width).step_by(width) {
-        slices_to_search.push((y..y+width).collect::<Vec<usize>>());
+        slices_to_search.push(Box::new(y..y+width));
         // println!("{:#?}", slices_to_search.last().unwrap());
     }
 
     // vertical slices.
     for x in 0..width {
-        slices_to_search.push((x..height*width).step_by(width).collect::<Vec<usize>>());
+        slices_to_search.push(Box::new((x..height*width).step_by(width)));
         // println!("{:#?}", slices_to_search.last().unwrap());
     }
 
     // diagonal to the left slices - top half
     for x in 0..width {
-        slices_to_search.push((x..1+(x*width)).step_by(width - 1).collect::<Vec<usize>>());
+        slices_to_search.push(Box::new((x..1+(x*width)).step_by(width - 1)));
         // println!("{:#?}", slices_to_search.last().unwrap());
     }
 
     // diagonal to the left slices - bottom half
     for y in (width * 2 - 1..width*height).step_by(width) {
-        slices_to_search.push((y..width*height).step_by(width - 1).collect::<Vec<usize>>());
+        slices_to_search.push(Box::new((y..width*height).step_by(width - 1)));
         // println!("{:#?}", slices_to_search.last().unwrap());
     }
 
 
     // diagonal to the right slices - top half
     for x in 0..width {
-        slices_to_search.push((x..((width - x)*width)).step_by(width + 1).collect::<Vec<usize>>());
+        slices_to_search.push(Box::new((x..((width - x)*width)).step_by(width + 1)));
         // println!("{:#?}", slices_to_search.last().unwrap());
     }
 
     for (i, y) in (width..width*height).step_by(width).enumerate() {
-        slices_to_search.push((y..y+(width - i - 1) * width).step_by(width + 1).collect::<Vec<usize>>());
+        slices_to_search.push(Box::new((y..y+(width - i - 1) * width).step_by(width + 1)));
         // println!("{:#?}", slices_to_search.last().unwrap());
     }
 
 
     let mut matches = 0;
-    let window: &mut [u8] = &mut vec![0; pattern_size];
 
     // Loops through of the slices created.
-    for slice in slices_to_search.iter() {
-        // Checks if the slice is big enough.
-        if slice.len() < pattern_size {
-            continue;
-        }
-
-        // constructs a sliding window the size of the pattern across the slice.
-        for window_indices in slice.windows(pattern_size) {
-
-            // extracts the desired elements into window.
-            window_indices.iter().enumerate().for_each(|(enumeration, &idx)| window[enumeration] = letters[idx]);
-
-            // checks to see if there is a match.
-            if window == pattern || window == rpattern {
-                matches += 1;
-            }
-        }
+    for slice in slices_to_search.iter_mut() {
+        let selection: Vec<u8> = slice.map(|idx| letters[idx]).collect();
+        matches += selection.windows(pattern_size).filter(|&w| w == pattern || w == rpattern).count();
     }
 
     format!("Matches: {matches}")
@@ -108,60 +93,52 @@ fn part2(input: &str) -> String {
     let pattern_size = pattern.len();
     let mut pattern_matches: Vec<usize> = vec![0; letters.len()];
 
-    let mut slices_to_search:Vec<Vec<usize>> = Vec::new();
-
-    // // horizontal slices.
-    // for y in (0..height*width).step_by(width) {
-    //     slices_to_search.push((y..y+width).collect::<Vec<usize>>());
-    //     // println!("{:#?}", slices_to_search.last().unwrap());
-    // }
-
-    // // vertical slices.
-    // for x in 0..width {
-    //     slices_to_search.push((x..height*width).step_by(width).collect::<Vec<usize>>());
-    //     // println!("{:#?}", slices_to_search.last().unwrap());
-    // }
+    let mut slices_to_search:Vec<Box<dyn Iterator<Item = usize>>> = Vec::with_capacity(2 * (width + height));
 
     // diagonal to the left slices - top half
     for x in 0..width {
-        slices_to_search.push((x..1+(x*width)).step_by(width - 1).collect::<Vec<usize>>());
+        slices_to_search.push(Box::new((x..1+(x*width)).step_by(width - 1)));
         // println!("{:#?}", slices_to_search.last().unwrap());
     }
 
     // diagonal to the left slices - bottom half
     for y in (width * 2 - 1..width*height).step_by(width) {
-        slices_to_search.push((y..width*height).step_by(width - 1).collect::<Vec<usize>>());
+        slices_to_search.push(Box::new((y..width*height).step_by(width - 1)));
         // println!("{:#?}", slices_to_search.last().unwrap());
     }
 
 
     // diagonal to the right slices - top half
     for x in 0..width {
-        slices_to_search.push((x..((width - x)*width)).step_by(width + 1).collect::<Vec<usize>>());
+        slices_to_search.push(Box::new((x..((width - x)*width)).step_by(width + 1)));
         // println!("{:#?}", slices_to_search.last().unwrap());
     }
 
     for (i, y) in (width..width*height).step_by(width).enumerate() {
-        slices_to_search.push((y..y+(width - i - 1) * width).step_by(width + 1).collect::<Vec<usize>>());
+        slices_to_search.push(Box::new((y..y+(width - i - 1) * width).step_by(width + 1)));
         // println!("{:#?}", slices_to_search.last().unwrap());
     }
 
 
-    let window: &mut [u8] = &mut vec![0; pattern_size];
-
-    for slice in slices_to_search.iter() {
-        if slice.len() < pattern_size {
+    for slice in slices_to_search.iter_mut() {
+        let indices: Vec<usize> = slice.collect();
+        if indices.len() < pattern_size {
             continue;
         }
 
-        for window_indices in slice.windows(pattern_size) {
-            window_indices.iter().enumerate().for_each(|(enumeration, &idx)| window[enumeration] = letters[idx]);
-
-            // same as part 1. But counts matches per location.
-            if window == pattern || window == rpattern {
-                pattern_matches[window_indices[window_indices.len() / 2]] += 1;
-            }
-        }
+        let selection: Vec<u8> = indices.iter().map(|&idx| letters[idx]).collect();
+        selection.windows(pattern_size)
+            .enumerate()
+            .filter_map(|(i, w)| {
+                if w == pattern || w == rpattern {
+                    Some(i)
+                } else {
+                    None
+                }
+            })
+            .for_each(|i| {
+                pattern_matches[indices[i + pattern_size / 2]] += 1;
+            });
     }
 
     // Once found all matches looks for all places with more than 1 match. = x.
