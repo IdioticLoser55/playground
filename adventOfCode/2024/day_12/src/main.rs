3f201885506ced69e3e2696178c626ad6493202d
Idiot_icLoser55 <IdioticLoser55@gmail.com>
2024-12-12 22:57:34 +0000

    improved by counting corners.

--- adventOfCode/2024/day_12/src/main.rs
+++ adventOfCode/2024/day_12/src/main.rs
@@ -154,47 +154,167 @@ fn me(input: &str) -> String {
         let mut sides = perimeter;
         // if area is 1 sides and perimeter are the same.
         if area != 1 {
+            // Could also look for corners. by windowing a 2 x 2 grid over the data.
+            // Look for all the possible cases for say the top left being your region.
+            // and whether the other 3 are in the region or not. Then turn that into a binary
+            // pattern.
+            // And match on the binary patterns that result in a corner.
 
-            // search left to right, then top to bottom.
-            // Pass in the flags for the opposite directions.
-            // DIRS[0] = RIGHT VEC; 4 = DOWN FLAG; 8 = UP FLAG;
-            // DIRS[3] = DOWN VEC; 1 = RIGHT FLAG; 2 = LEFT FLAG;
-            for (dir, dir_flags) in [(DIRS[0], 4 | 8), (DIRS[3], 1 | 2)] {
-                // for each point in the region.
-                for &i in region_indicies.as_slice() {
-                    // checks to see if visited from the flag directions.
-                    if visited_from[i] & dir_flags == dir_flags {
-                        continue;
-                    }
 
-                    // works out the coordinates of the adjacent pos.
-                    let next_pos = &Point::from_idx(i, &bounds) + &dir;
-                    if !next_pos.is_within_bounds(&bounds) {
-                        continue;
-                    }
+            // New method. Looks for and counts forners.
+            let num_corners: usize = region_indicies
+                .iter()
+                .map(|&i| {
+                    let visited_from = visited_from[i];
 
-                    // index of adjacent pos.
-                    let next_i = next_pos.to_idx(&bounds);
-                    // is index part of region.
-                    if !region_indicies.contains(&next_i) {
-                        continue;
-                    }
+                    // const DIRS: [Point; 4] = [
+                    //     Point{x: 1, y: 0},   0001 - Move right
+                    //     Point{x: -1, y: 0},  0010 - Move left
+                    //     Point{x: 0, y: 1},   0100 - Move down
+                    //     Point{x: 0, y: -1},  1000 - Move up.
+                    // ];
 
-                    // Or the two spaces. To find all the directions they were visited from.
-                    // Apply NOT. To get all the directions they weren't visited from.
-                    // Mask with dir_flags for the current directions being tested.
-                    // If neither side was visited from one of the directions being tested
-                    // then we can subtract 1 from sides/perimeter because they are adjacent.
-                    // Because dir_flags contains the two opposite directions,
-                    // we check both directions at the same time. And then count the number of ones
-                    // in the result.
-                    let bin_op = !(visited_from[i] | visited_from[next_i]) & dir_flags;
-                    sides -= (bin_op).count_ones() as usize;
-                }
+                    let num = match visited_from.count_ones() {
+                        0 => 4, // Single square 4 corners.
+                        1 => 2, // end of line. two corners.
+                        2 => {
+                            // If two parallel lines, then no corner.
+                            if visited_from == 0b1100 || visited_from == 0b0011 {0} else {
+                                // need to check opposite diagonal for inside corner.
+                                // visited from tracks the directions from which a cell moved into
+                                // this one.
+                                // So we can find the opposite diagonal by inverting visited_from.
+                                // And then using it to selct the directions from DIRS.
+                                
+                                let pos = Point::from_idx(i, &bounds);
+                                let mut delta = Point{x: 0, y: 0};
+                                let mut visited_from = !visited_from;
+                                for i in 0..4 {
+                                    if visited_from & 1 == 1 {
+                                        delta = &delta + &DIRS[i];
+                                    }
+                                    visited_from = visited_from >> 1;
+                                }
 
-            }
+                                if grid[(&pos + &delta).to_idx(&bounds)] != plot {
+                                    //inside corner found.
+                                    2
+                                } else {
+                                    // only an outside corner.
+                                    1
+                                }
+                        }},
+                        3 => {
+                            // T shape. Need to check if under the arms is inside corner.
+                            let pos = Point::from_idx(i, &bounds);
+
+                            // Check if T is upright or Sideways.
+                            if visited_from & 0b1100 == 0b1100 {
+                                // Sideways T
+                                
+                                // Get the horizontal direction of the T and select the opposite
+                                // one.
+                                let horizontal_dir = if visited_from & 0b0011 == 1 {
+                                    DIRS[1]
+                                } else {
+                                    DIRS[0]
+                                };
+
+                                // Check both up and down.
+                                let mut count = 0;
+                                if grid[(&(&pos + &horizontal_dir) + &DIRS[2]).to_idx(&bounds)] != plot {
+                                    count += 1;
+                                }
+                                if grid[(&(&pos + &horizontal_dir) + &DIRS[3]).to_idx(&bounds)] != plot {
+                                    count += 1;
+                                }
+
+                                count
+                            } else {
+                                // Upright T
+                                
+                                // Get the vertical direction of the T and select the opposite.
+                                let vertical_dir = if visited_from & 0b1100 == 0b0100 {
+                                    DIRS[3]
+                                } else {
+                                    DIRS[2]
+                                };
+
+                                // Check both left and right.
+                                let mut count = 0;
+                                if grid[(&(&pos + &vertical_dir) + &DIRS[0]).to_idx(&bounds)] != plot {
+                                    count += 1;
+                                }
+                                if grid[(&(&pos + &vertical_dir) + &DIRS[1]).to_idx(&bounds)] != plot {
+                                    count += 1;
+                                }
+
+                                count
+                            }},
+                        4 => {
+                            // Central point. Need to check the four diags for inside corners.
+                            let pos = Point::from_idx(i, &bounds);
+                            // Check diags from here. Each diag not part of the region is a corner.
+                            [Point{x: 1, y: 1}, Point{x: 1, y: -1}, Point{x: -1, y: 1}, Point{x: -1, y: -1}]
+                                .iter()
+                                .filter(|&p| {
+                                    // Don't need to check within bounds.
+                                    // Can't hit all 4 from the border.
+                                    grid[(p + &pos).to_idx(&bounds)] != plot
+                                })
+                                .count()
+                        },
+                        _ => panic!("ARRRGGGHHHH"),
+                    };
+                    return num;
+                })
+                .sum();
+
+            sides = num_corners;
+
+
+
+
+
+            // // search left to right, then top to bottom.
+            // // Pass in the flags for the opposite directions.
+            // // DIRS[0] = RIGHT VEC; 4 = DOWN FLAG; 8 = UP FLAG;
+            // // DIRS[3] = DOWN VEC; 1 = RIGHT FLAG; 2 = LEFT FLAG;
+            // for (dir, dir_flags) in [(DIRS[0], 4 | 8), (DIRS[3], 1 | 2)] {
+            //     // for each point in the region.
+            //     for &i in region_indicies.as_slice() {
+            //         // checks to see if visited from the flag directions.
+            //         if visited_from[i] & dir_flags == dir_flags {
+            //             continue;
+            //         }
+
+            //         // works out the coordinates of the adjacent pos.
+            //         let next_pos = &Point::from_idx(i, &bounds) + &dir;
+            //         if !next_pos.is_within_bounds(&bounds) {
+            //             continue;
+            //         }
+
+            //         // index of adjacent pos.
+            //         let next_i = next_pos.to_idx(&bounds);
+            //         // is index part of region.
+            //         if !region_indicies.contains(&next_i) {
+            //             continue;
+            //         }
+
+            //         // Or the two spaces. To find all the directions they were visited from.
+            //         // Apply NOT. To get all the directions they weren't visited from.
+            //         // Mask with dir_flags for the current directions being tested.
+            //         // If neither side was visited from one of the directions being tested
+            //         // then we can subtract 1 from sides/perimeter because they are adjacent.
+            //         // Because dir_flags contains the two opposite directions,
+            //         // we check both directions at the same time. And then count the number of ones
+            //         // in the result.
+            //         let bin_op = !(visited_from[i] | visited_from[next_i]) & dir_flags;
+            //         sides -= (bin_op).count_ones() as usize;
+            //     }
+
+            // }
         }
-
 
         p1 += area * perimeter;
         p2 += area * sides;
