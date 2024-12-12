1e7dfffb0125e6836062a7fe2b828a0d58750913
Idiot_icLoser55 <IdioticLoser55@gmail.com>
2024-12-12 17:27:46 +0000

    completed day 12.

--- adventOfCode/2024/day_12/src/main.rs
+++ adventOfCode/2024/day_12/src/main.rs
@@ -48,7 +48,6 @@ impl Point {
         Point{x: idx as i64 % bounds.x, y: idx as i64 / bounds.x}
     }
 }
-
 const DIRS: [Point; 4] = [
     Point{x: 1, y: 0},
     Point{x: -1, y: 0},
@@ -57,7 +56,7 @@ const DIRS: [Point; 4] = [
 ];
 
 fn main() {
-    let file_path = "test2.txt";
+    let file_path = "input.txt";
     let input = fs::read_to_string(file_path).unwrap();
 
     println!("ME: \n{}\n", bench(&input, me));
@@ -83,12 +82,13 @@ fn me(input: &str) -> String {
     let bounds = Point{x: lines[0].len() as i64, y: lines.len() as i64};
 
     let mut visit_count: Vec<usize> = vec![0; grid.len()];
-    visit_count.fill(0);
+    let mut visited_from: Vec<u8> = vec![0; grid.len()];
 
     let mut bfs_queue = Vec::new();
-    let mut region_idx = Vec::new();
+    let mut region_indicies = Vec::new();
 
     let mut p1 = 0;
+    let mut p2 = 0;
 
     for (i, &plot) in grid.iter().enumerate() {
         // println!("FOR: i: {i}, p: {plot}");
@@ -96,10 +96,10 @@ fn me(input: &str) -> String {
 
         let pos = Point::from_idx(i, &bounds);
         // println!("POINT: {:?}", pos);
-        bfs_queue.push(pos);
-        region_idx.clear();
+        bfs_queue.push((pos, 0));
+        region_indicies.clear();
 
-        while let Some(pos) = bfs_queue.pop() {
+        while let Some((pos, dir_flag)) = bfs_queue.pop() {
             // println!("Loop Pos: {:?}", pos);
 
             if !pos.is_within_bounds(&bounds) {
@@ -116,14 +116,15 @@ fn me(input: &str) -> String {
 
 
             visit_count[i] += 1;
+            visited_from[i] |= dir_flag;
             if visit_count[i] > 1 {
                 // println!("Already been");
                 continue;
             }
 
-            region_idx.push(i);
-            for dir in DIRS.iter() {
-                bfs_queue.push(dir + &pos)
+            region_indicies.push(i);
+            for (dir_idx, dir) in DIRS.iter().enumerate() {
+                bfs_queue.push((dir + &pos, 1 << dir_idx))
             }
             
             // println!("Queue: {:?}", bfs_queue);
@@ -134,19 +135,58 @@ fn me(input: &str) -> String {
         visit_count[i] -= 1;
         // println!("Visit_count: {:?}", visit_count);
 
-        let area = region_idx.len();
-        let perimeter: usize = region_idx
+        let area = region_indicies.len();
+        let perimeter: usize = region_indicies
             .iter()
             .map(|&idx| 4 - visit_count[idx])
             .sum();
 
+        let mut sides = perimeter;
+        // const DIRS: [Point; 4] = [
+        //     Point{x: 1, y: 0},       1
+        //     Point{x: -1, y: 0},      2
+        //     Point{x: 0, y: 1},       4
+        //     Point{x: 0, y: -1},      8
+        // ];
+
+        for (dir, dir_flags) in [(DIRS[0], 4 | 8), (DIRS[3], 1 | 2)] {
+            // println!("DIR: {:?}, flags: {:#06b}", dir, dir_flags);
+            for &i in region_indicies.as_slice() {
+                // println!("I: {i}, visited_from[i]: {:#06b}", visited_from[i]);
+
+                if visited_from[i] & dir_flags == dir_flags {
+                    // println!("Already been visited from this dir.");
+                    continue;
+                }
+
+                let next_pos = &Point::from_idx(i, &bounds) + &dir;
+                if !next_pos.is_within_bounds(&bounds) {
+                    // println!("Not in bounds.");
+                    continue;
+                }
+
+                let next_i = next_pos.to_idx(&bounds);
+                // println!("Next i: {next_i}");
+                if !region_indicies.contains(&next_i) {
+                    // println!("Not in region");
+                    continue;
+                }
+
+                // println!("Sides: {sides}");
+                // println!("visited_from[i]:  {:#06b}", visited_from[i]);
+                // println!("visited_from[ni]: {:#06b}", visited_from[next_i]);
+                // println!("dir_flags:        {:#06b}", dir_flags);
+                let bin_op = !(visited_from[i] | visited_from[next_i]) & dir_flags;
+                // println!("bin operation:    {:#06b}", bin_op);
+                sides -= (bin_op).count_ones() as usize;
+                // println!("Sides: {sides}");
+            }
+        }
+
         p1 += area * perimeter;
-        // println!("I: {i}, PLOT: {plot}, area: {area}, Perim: {perimeter}");
+        p2 += area * sides;
+        // println!("Visited From: {:?}", visited_from);
     }
 
-    println!("Part 1: {p1}");
-
-
-
-    "".to_string()
+    format!("Part 1: {p1}\nPart 2: {p2}")
 }
